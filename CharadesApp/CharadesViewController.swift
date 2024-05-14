d5db1a01f6de3eb3c2641eb73e6206472004348b
cfuntila <cfuntila@ucsc.edu>
2024-05-13 18:28:10 -0700

    Added CoreMotion to detect phone flips

--- CharadesApp/CharadesViewController.swift
+++ CharadesApp/CharadesViewController.swift
@@ -6,26 +6,81 @@
 //
 
 import UIKit
+import CoreMotion
 
 class CharadesViewController: UIViewController {
 
     @IBOutlet weak var animalLabel: UILabel!
     @IBOutlet weak var timerLabel: UILabel!
+    let motionManager = CMMotionManager()
     
     let words = ["Frog", "Bird", "Dog", "Cat", "Snake", "Bunny", "Lion", "Monkey", "Unicorn", "Shark"]
+    var wordIndex = 0
+    var isDebouncing = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        animalLabel.text = words[0]
-
+        showNextWord()
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(runTimer), userInfo: nil, repeats: true)
+        motionManager.accelerometerUpdateInterval = 0.1
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in
+            if data != nil {
+                self.handleTilt(data!)
+            }
+        }
     }
     
-    @objc func runTimer() {
-       print("a second later")
+    func handleTilt(_ data: CMAccelerometerData) {
+        if isDebouncing == true {
+            return
+        }
+        
+        let acceleration = data.acceleration
+        let currentPosition = acceleration.z
+        
+        let upTilt = -0.8
+        let downTilt = 0.8
+        
+        if currentPosition < upTilt {
+//            tilted upward, skip
+            print("Skip")
+            waitForBounceBack()
+            showNextWord()
+        }
+        if currentPosition > downTilt {
+            print("Pass")
+            waitForBounceBack()
+            showNextWord()
+        }
+        
     }
-
-
+    
+    func showNextWord() {
+        if wordIndex >= words.count {
+            return
+        } else {
+            animalLabel.text = words[wordIndex]
+            wordIndex += 1
+        }
+        
+    }
+    
+    func waitForBounceBack() {
+        isDebouncing = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.isDebouncing = false
+        }
+    }
+    
+    func triggerDebounce() {
+               // Set debouncing state
+               isDebouncing = true
+               
+               // Wait for 1 second before allowing another action
+               DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                   self.isDebouncing = false
+               }
+           }
 }
 
